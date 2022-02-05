e1981a43a4a9bca11576f421854e9654c6db412e
Diego Arthur <digo.arthur@hotmail.com>
2022-02-05 15:09:01 -0300

    add comment code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,15 +10,15 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
 
-                    <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+                    <link rel="shortcut icon" href="favicon.png" type="image/png" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
                         rel="stylesheet" />
 
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 
-import Head from 'next/head';
+//************************************************************ */
+// PAGE HOME
+//************************************************************ */
 
+// PARA RODAR a aplicação
+//npm run dev
+
+
+//UTILIZADO PARA ACESSAR A TAG HEAD DO HTML E INSERIR, AQUI, O TITLE DA PAGE.
+import Head from 'next/head'; 
+
+//IMPORTAÇÃO DE COMPONENTES
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
-
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
-
-// PARA RODAR 
-//npm run dev
-
+//IMPORTAÇÃO CSS
 import styles from '../styles/pages/Home.module.css';
+
+
 export default function Home() {
   return (
 
